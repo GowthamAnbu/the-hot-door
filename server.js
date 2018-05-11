16c3f91165f95dbc008c4b38e620d789546603b6
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-05-11 14:05:46 +0530

    eslint,nodemon configured

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express'), // the king 
-chalk = require('chalk'),  // chalk is for painting the console with colors :)
-debug = require('debug')('server'), // server is the name of the 
-morgan = require('morgan'), // the logger for the API traffic
-path = require('path') // path is used to solve cross platform (windows,linux,macos, etc..) string path match error
-app = express();
-const port = process.env.PORT || 3000;
+const express = require('express'); // the king
+const chalk = require('chalk'); // chalk is for painting the console with colors :)
+const debug = require('debug')('server'); // server is the name of the
+const morgan = require('morgan'); // the logger for the API traffic
+const path = require('path'); // path is used to solve cross platform (windows,linux,macos, etc..) string path match error
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(morgan('dev'));
 
