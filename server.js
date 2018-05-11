ca6da13a20686f68c94f246608fd747d8c7a2cd2
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-05-11 13:22:53 +0530

    static files configured

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ path = require('path') // path is used to solve cross platform (windows,linux,ma
 app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
-app.get('/', (request, response) => response.sendFile(path.join(__dirname,'/views/index.html')));
+app.get('/', (request, response) => response.sendFile(path.join(__dirname, '/views/index.html')));
 
 app.listen(port, () => debug(`listening on http://localhost:${chalk.green(port)}`));
